a2b3ecdc32d2c690484127f8f7f6b3535acd76d4
PaboXZ <pabo@wp.pl>
2024-06-27 13:32:15 +0200

    Added admin controle

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Recipe;
+use App\Entity\User;
 use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -11,13 +12,20 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        //Adding dummy data
         $this
             ->addRecipe($manager, 'recipe1', 'content1', ['breakfast', 'dinner'])
             ->addRecipe($manager, 'recipe2', 'content2', ['breakfast', 'dinner'])
             ->addRecipe($manager, 'recipe3', 'content3', ['pizza', 'dinner'])
             ->addRecipe($manager, 'recipe4', 'content4', ['pizza', 'dinner']);
+
+        //Admin acc
+        $admin = new User;
+        $admin
+            ->setRoles(['USER_ADMIN'])
+            ->setUsername('admin')
+            ->setPassword('$2y$13$K6ismNdXeb3D9jrMfiK6GONfo88SVFzjA9w1JSAiLi2iXbQ2HuzcO'); //Set default password: 'admin'
+        $manager->persist($admin);
 
         $manager->flush();
     }
@@ -39,4 +47,5 @@ class AppFixtures extends Fixture
         $manager->persist($recipe);
         return $this;
     }
+
 }
